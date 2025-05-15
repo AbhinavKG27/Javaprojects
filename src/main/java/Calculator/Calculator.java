4faf4ae5764395bad7582ae78cd21b63d7d26071
AbhinavKG27 <157568967+AbhinavKG27@users.noreply.github.com>
2025-05-15 13:09:00 +0530

    Add files via upload

---
+++ src/main/java/Calculator/Calculator.java
@@ -0,0 +1,51 @@
+package Calculator;
+
+import java.util.Scanner;
+
+public class Calculator {
+
+	public static void main(String[] args) {
+		// result will be in float form
+		float res = 0;
+
+		Scanner userInput = new Scanner(System.in);
+		System.out.println("Enter the 1st number : ");
+		float num1 = userInput.nextFloat();
+
+		System.out.println("Enter the 2nd number :");
+		float num2 = userInput.nextFloat();
+
+		// print basic mathematical operators
+		System.out.println("Enter the operation :");
+		System.out.println("1. +");
+		System.out.println("2. -");
+		System.out.println("3. *");
+		System.out.println("4. /");
+
+		// using the scanner class to take input which will only be in int form
+		int optr = userInput.nextInt();
+
+		// using control statement to print the operator statements and also handle some
+		// errors statements
+		if (optr > 4) {
+			System.out.println("Error");
+		} else {
+			if (optr == 1) {
+				res = num1 + num2;
+			} else if (optr == 2) {
+				res = num1 - num2;
+			} else if (optr == 3) {
+				res = num1 * num2;
+			} else {
+				res = num1 / num2;
+
+			}
+			System.out.println("The result is " + res);
+		}
+
+		// close scanner to avoid data leak
+		userInput.close();
+
+	}
+
+}
